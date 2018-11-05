3cf1c87a89b1cccfccec7fe791f42f658e8de9c9
Legendary <Legendary-tony>
2018-11-05 23:55:01 +0100

    Basic Routes To be Implemented

--- server/routes/web.php
+++ server/routes/web.php
@@ -11,6 +11,47 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Landing Page (Home Page)
+Route::get('/', 'HomepageController@index')->name('homepage');
+
+//User Profile
+Route::get('/User-profile', function(){
+	return view();
 });
+
+//Login
+Route::post('/login', function(){
+
+});
+
+
+//Sign Up
+Route::post('/sign-up', function(){
+
+});
+
+//Post Comment
+Route::post('/post-comment', function(){
+
+});
+
+//Upload Files
+Route::post('/Upload', function(){
+
+});
+
+//Update User Profile
+Route::put('/update-user-profile', function(){
+
+});
+
+//Reset User Password
+Route::put('/reset-user-password', function(){
+
+});
+
+//Delete User Account
+Route::delete('/delete-user-account', function(){
+
+});
+
